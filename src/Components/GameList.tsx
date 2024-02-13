5715bbb8f545143996829721d3c1fb080d796933
Costas Ioannou <ioannou.dev@gmail.com>
2024-02-13 15:07:53 +0200

    fix reactivity issue

--- src/Components/GameList.tsx
+++ src/Components/GameList.tsx
@@ -4,18 +4,26 @@ import {Text, View, StyleSheet, Button} from 'react-native';
 import { BoardGame } from '../model/model';
 import {deleteBoardGame, increasePlayers} from "../model/database";
 
+function GameListItem ({game}: {game: BoardGame}) {
+  return (
+      <View key={game.title} style={styles.gameRow}>
+    <View style={styles.button}>
+      <Button title='DEL' onPress={() => deleteBoardGame(game)} />
+      <Button title='INC' onPress={() => increasePlayers(game)}/>
+    </View>
+    <Text>{game.title} - </Text>
+    <Text>{game.minPlayers}+</Text>
+  </View>
+  )
+}
+
+const EnhancedGameListItem = withObservables(['game'], ({ game }) => ({game}))(GameListItem);
+
 function GameListComponent({ games }: { games: BoardGame[] }) {
   return (
     <View>
       {games.map((game) => (
-        <View key={game.title} style={styles.gameRow}>
-          <View style={styles.button}>
-            <Button title='DEL' onPress={() => deleteBoardGame(game)} />
-            <Button title='INC' onPress={() => increasePlayers(game)}/>
-          </View>
-          <Text>{game.title} - </Text>
-          <Text>{game.minPlayers}+</Text>
-        </View>
+          <EnhancedGameListItem key={game.title} game={game}/>
       ))}
     </View>
   );
